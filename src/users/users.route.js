efeace7bc6096bb56e9f973b3dffa98d3eb68b4a
Bruno Oliveira <105464045+Bruintut@users.noreply.github.com>
2022-08-30 15:52:35 -0300

    alter route of character

--- src/users/users.route.js
+++ src/users/users.route.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const userController = require("./users.controller.js");
 
-router.post("/", userController.createUserController);
+router.post("/create", userController.createUserController);
 router.get("/", userController.findAllUserController);
 
 module.exports = router;
